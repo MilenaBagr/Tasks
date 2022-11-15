4c374069873758ed5a5c3db5d9c022dfe8f77c9c
Milena <bagramanmilena@gmail.com>
2022-11-15 16:45:18 +0300

    Выдвем названия дня недели по номеру

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая
+// Будет выдавать название дня недели по заданному номеру.
+// Например :
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Введите номер дня недели: ");
+int weekDay = Convert.ToInt32(Console.ReadLine());
+if (weekDay == 1) Console.WriteLine("Понедельник");
+else if (weekDay == 2) Console.WriteLine("Вторник");
+else if (weekDay == 3) Console.WriteLine("Среда");
+else if (weekDay == 4) Console.WriteLine("Четверг");
+else if (weekDay == 5) Console.WriteLine("Пятница");
+else if (weekDay == 6) Console.WriteLine("Суббота");
+else if (weekDay == 7) Console.WriteLine("Воскресенье");
+else Console.WriteLine ("Такого дня недели не существует");
