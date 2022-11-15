d13acbcb7b3d691afcd67289db1a107a2a75f5db
Milena <bagramanmilena@gmail.com>
2022-11-15 15:42:41 +0300

    Решили задачу 01, вводим два числа и проверяем, является ли первое число квадратом второго

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая
+
+// 1. на вход принимает два числа
+// 2.  проверяет, является ли первое число квадратом второго.
+
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите первое число: ");
+int squareNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num * num == squareNum) 
+{
+    Console.WriteLine($"Число {squareNum} является квадратом числа {num}: ");
+}
+else
+{
+    Console.WriteLine($"Число {squareNum} НЕ является квадратом числа {num}: ");
+}
