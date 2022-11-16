ee656c23a3307ad2757a407cb542c7c8a13fa9f0
Milena <bagramanmilena@gmail.com>
2022-11-16 15:46:20 +0300

    Написали программу, которая на выходе показывает все четные числа

---
+++ Task08/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 8: Напишите программу, которая
+
+// 1. на вход принимает число (N)
+// 2. а на выходе показывает все чётные числа от 1 до N.
+
+//  5 -> 2, 4
+//  8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= num)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count}");
+    }
+    else
+    {
+        Console.Write(" ");
+    }
+    count = count + 1;
+}
