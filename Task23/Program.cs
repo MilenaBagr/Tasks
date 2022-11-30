bf94549736799f4198a18d88c9b4c85e0b0830df
Milena <bagramanmilena@gmail.com>
2022-11-30 19:50:44 +0300

    Принимает на вход число N и выдает таблицу кубов

---
+++ Task23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23
+
+// Напишите программу, 
+// 1.которая принимает на вход число (N)
+// 2. выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void TableCub(int num)
+{
+    for(int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i} | {i * i * i}");
+
+    }
+}
+TableCub(number);
