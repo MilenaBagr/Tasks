4e00f72f5693b0f57c931d83311ebaab9b9881c1
Milena <bagramanmilena@gmail.com>
2022-11-30 19:47:14 +0300

    Программа выдает вторую цифру

---
+++ Task10/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 10: Напишите программу, которая 
+
+// 1. принимает на вход трёхзначное число 
+// 2. на выходе показывает вторую цифру этого числа
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondNum(int num)
+{
+    if (num > 99 && num < 1000)
+    {
+        num = num / 10 % 10;
+        return num;
+    }
+    return -1;
+}
+int res = SecondNum(number);
+
+string result = res > -1 ? $"Вторая цифра числа {number} --> {res}" : "Введено не трехзначное число";
+Console.WriteLine(result);
