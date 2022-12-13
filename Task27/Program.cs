c452cd5a24f8fe96d5c38c2f4be76f9198281d3b
Milena <bagramanmilena@gmail.com>
2022-12-13 09:49:26 +0300

    Выводим сумму цифр в числе

---
+++ Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) number = -number;
+int Sum(int num)
+{
+    int summa = 0;
+    while ( num > 0)
+    {
+        summa = summa + num % 10;
+        num = num / 10;
+    }
+    return summa;
+}
+
+int sum = Sum(number);
+string result = ( sum > 0 ) ? $"Сумма цифр в числе {number} = {sum}" : "Введено неверное значение.";
+Console.WriteLine(result);
