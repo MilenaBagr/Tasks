8c861e7c06ab92efcb1dafda1c596c71ca60780e
Milena <bagramanmilena@gmail.com>
2023-02-21 15:08:02 +0300

    Задача 48. Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле Аmn = m + n.

---
+++ Task48/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 48. Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле Аmn = m + n.
+// Выведите полученный массив на экран.
+
+int[,] CreateMatrix(int rows, int columns)
+{
+    int[,] matr = new int[rows, columns];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = i + j;
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j], 2}, ");
+            else Console.Write($"{matr[i, j], 2} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int [,] matrix = CreateMatrix(50,50);
+PrintMatrix(matrix);
