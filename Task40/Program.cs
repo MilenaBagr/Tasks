443ab0ad135b19346f8b8077c640cd2dbd0fb25d
Milena <bagramanmilena@gmail.com>
2023-02-21 14:58:54 +0300

    Задача 40. Напишите программу, которая принимаетна вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины

---
+++ Task40/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 40. Напишите программу, которая принимает 
+// на вход три числа и проверяет, может ли существовать 
+// треугольник с сторонами такой длины.
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+bool Triagnle(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < b + a;
+}
+
+Console.WriteLine(Triagnle(number1, number2, number3) ? "Треугольник может существовать" : "Треугольник не может существовать");
