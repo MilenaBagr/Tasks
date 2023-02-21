489a50bf70daa8dd5b3124031a9b28b8ce0348ea
Milena <bagramanmilena@gmail.com>
2023-02-21 15:06:53 +0300

    Задача 46. Задайте двумерный массив размером m *n, заполненный случайными целыми числами.

---
+++ Task46/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 46. Задайте двумерный массив размером m *n, 
+// заполненный случайными целыми числами.
+
+int[,] NewRandomMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j], 4}, ");
+            else Console.Write($"{matr[i, j], 4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] array2D = NewRandomMatrix(3, 4, - 50, 50);
+PrintMatrix(array2D);
