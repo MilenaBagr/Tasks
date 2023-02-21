ac6f1c1360dae671913e530d930c871be1de7dab
Milena <bagramanmilena@gmail.com>
2023-02-21 14:52:34 +0300

    Задача 33:Напишите программу, которая определяет, присутствует ли заданное число в массиве.

---
+++ Task33/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 33: 
+// 1. Задайте массив. 
+// 2. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+
+    Console.Write("]");
+
+}
+
+bool PresenceNumb(int [] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) return true;
+    }
+    return false;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array1 = CreateArrayRndInt( 5, 1, 10);
+PrintArray(array1);
+Console.WriteLine();
+bool trueFalse = PresenceNumb(array1, number);
+Console.WriteLine("Заданное число присутствует в массиве?");
+Console.WriteLine(trueFalse == true ? "Да" : "Нет");
